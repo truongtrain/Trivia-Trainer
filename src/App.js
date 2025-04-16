70aac85b9aa7b429eb952e3f87ac4ae94852e190
truongtrain <alan.truong@utah.edu>
2025-04-16 19:45:30 +1200

    fix opponentAnswer() logic

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ const App = () => {
   const [board, setBoard] = useState(null);
 
   useEffect(() => {
-    fetch('http://localhost:5000/game/9143')
+    fetch('http://localhost:5000/game/9144')
       .then((res) => res.json())
       .then((data) => {
         showData = data;
